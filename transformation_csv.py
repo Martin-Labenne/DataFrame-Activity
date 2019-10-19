d90f7422fd1e8acc5992cc8bf1b2959c5a2044c5
Martin-Labenne <martin.labenne@isep.fr>
2019-10-19 16:50:11 +0200

    identification of issues about dates, and try to find some solution to delete outliers from the dataset

--- transformation_csv.py
+++ transformation_csv.py
@@ -6,14 +6,24 @@ Created on Thu Oct 17 15:18:38 2019
 """
 
 import pandas as pd
+import datetime as dt 
 
 births = pd.read_csv('births.csv')
-births = births.dropna() 
+#births = births.dropna() 
+
 date = {'date':[]}
 for i in range(len(births)):    # create date Serie  
-    date['date'].append(pd.to_datetime([births['year'][i],
-                                       births['month'][i],
-                                       births['day'][i]], yearfirst = True))
+    date['date'].append(dt.date(births['year'][i],
+                            births['month'][i],
+                            int(births['day'][i])))
 
 date = pd.DataFrame(date)
 
+del births['year']
+del births['month']
+del births['day']
+
+births_transformed = pd.merge(births, date, left_on=births.index,
+                              right_on = date.index)
+
+del births_transformed['key_0']
